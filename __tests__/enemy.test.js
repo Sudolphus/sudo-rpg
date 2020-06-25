76567b6ed4ec7f65e3ff73e84c6c89af8dda23ee
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-25 10:09:41 -0700

    give attributes to enemies

--- __tests__/enemy.test.js
+++ __tests__/enemy.test.js
@@ -5,14 +5,22 @@ import { Enemy } from './../src/enemies.js';
 describe ('RPG Enemies', ()=>{
   let game;
   let enemy;
-  
+  let goblin;
+
   beforeEach(()=>{
     game = new Game();
     enemy = new Enemy();
+    goblin = new Enemy('goblin', 5, 8);
   });
 
   test('should allow enemies to be added to the game', ()=>{
     game.addEnemy(enemy);
     expect(game.enemies[0]).toMatchObject(enemy);
+  });
+
+  test('enemies should have a name, damage value, and hp', ()=>{
+    expect(goblin.name).toBe('goblin');
+    expect(goblin.damageValue).toBe(5);
+    expect(goblin.hp).toBe(8);
   });
 });
--- src/enemies.js
+++ src/enemies.js
@@ -1,3 +1,7 @@
 export class Enemy {
-  
+  constructor(name, damageValue, hp) {
+    this.name = name;
+    this.damageValue = damageValue;
+    this.hp = hp;
+  }
 }
