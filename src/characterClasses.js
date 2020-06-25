c75156f5b8d6f28e7b332f70b7e7f3178e6213df
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-24 22:10:48 -0700

    create a player inventory

--- src/characterClasses.js
+++ src/characterClasses.js
@@ -7,6 +7,7 @@ export class Character {
     this.intelligence = intelligence;
     this.dexterity = dexterity;
     this.characterClass = characterClass;
+    this.inventory = [];
   }
 
   attack() {
