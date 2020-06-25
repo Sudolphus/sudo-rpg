ef493813cbf242be166671cb270e476141b88d33
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-24 21:23:17 -0700

    add abilities

--- src/characterClasses.js
+++ src/characterClasses.js
@@ -29,16 +29,34 @@ export class Warrior extends Character {
   constructor() {
     super(20, 10, 10, 3, 8, 'warrior');
   }
+  powerSmash() {
+    return this.strength*2;
+  }
+  doubleStrike() {
+    return (this.strength*.5)*2;
+  }
 }
 
 export class Wizard extends Character {
   constructor() {
     super(10, 20, 3, 10, 8, 'wizard');
   }
+  fireball() {
+    return this.intelligence;
+  }
+  lightningBolt() {
+    return this.intelligence*2;
+  }
 }
 
 export class Thief extends Character {
   constructor() {
     super(15, 15, 6, 5, 10, 'thief');
+  }
+  riposte() {
+    return this.dexterity;
+  }
+  sneakyStab() {
+    return this.dexterity;
   }
 }
